bce2e3d01690e95a2735c3121be1b53ea93522ac
noah-10 <noah.fryman.10@gmail.com>
2024-04-14 21:29:25 -0400

    Fixed style on resume and made responsive

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,11 @@
 import DownloadResume from "../components/resume/downloadResume"
 import Skills from "../components/resume/skills"
+import '../css/resume/resume.css';
 
 export default function Resume(){
     return (
         <>
-            <h1 className="text-center">Resume</h1>
+            <h1 className="text-center resume-header">Resume</h1>
             <DownloadResume />
             <Skills />
         </>
