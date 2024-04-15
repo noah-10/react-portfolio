d1c640ed6306393bbb3d6663df6eb4df4d4c047a
noah-10 <noah.fryman.10@gmail.com>
2024-04-15 16:08:00 -0400

    Made portfolio page responsive

--- src/components/portfolio/projectCards.jsx
+++ src/components/portfolio/projectCards.jsx
@@ -1,11 +1,45 @@
 import '../../css/portfolio/projectCards.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function ProjectCards(props) {
 
     const [hoveredIndex, setHoveredIndex] = useState(null);
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    });
+
+    useEffect(() => {
+        if(windowWidth < 426){
+            addDisplay();
+        }
+    });
+
+    const addDisplay = () => {
+        const cardText = document.querySelectorAll('.card-header');
+        cardText.forEach((card) => {
+            card.classList.remove('inactive-card');
+            card.style.opacity = 1;
+        });
+
+        const techText = document.querySelectorAll('.tech');
+        techText.forEach((tech) => {
+            tech.classList.remove('inactive-card');
+            tech.style.opacity = 1;
+        })
+    }
 
     return(
         <div className="project-container">
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,10 @@
 import ProjectContainer from "../components/portfolio/projectContainer"
+import '../css/portfolio/portfolio.css';
 
 export default function Portfolio(){
     return(
         <>
-            <h1 className="text-center">Portfolio</h1>
+            <h1 className="text-center portfolio-header">Portfolio</h1>
             <ProjectContainer />
         </>
         
