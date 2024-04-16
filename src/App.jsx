f873b74176b3a1af5c3f0cb9315d03af7541df24
noah-10 <noah.fryman.10@gmail.com>
2024-04-15 22:32:49 -0400

    Added a footer with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <>
         <Header />
         <Outlet />
+        <Footer />
     </>
   )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,27 @@
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '../css/footer.css';
+
+export default function Footer(){
+    return(
+        <div className="footer-container">
+            <ul className="links">
+                <li className="link-item">
+                    <a className="link" href="https://github.com/noah-10">
+                        <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                    </a>
+                </li>
+                <li className="link-item">
+                    <a className="link" href="https://www.linkedin.com/in/noah-fryman-68ab2a256/">
+                        <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                    </a>
+                </li>
+                <li className="link-item">
+                    <a className="link" href="https://www.instagram.com/noahh.10/">
+                        <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
